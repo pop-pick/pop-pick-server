4aa0ad5a13f55aae359d874b9c5c24a480d50e85
msj9965 <121001545+msj9965@users.noreply.github.com>
2026-09-08 01:14:03 +0900

    chore[#4]: Spring AI, VectorDB용 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.spring.dependency.management)
     alias(libs.plugins.ktlint)
 }
+val springAiVersion by extra("2.0.1")
 
 val jdkVersion: String = libs.versions.jdk.get()
 
@@ -32,11 +33,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation(libs.kotlin.reflect)
     implementation(libs.jackson.module.kotlin)
+    implementation("org.springframework.ai:spring-ai-starter-model-openai")
 
     // db
     runtimeOnly(libs.postgresql)
+    implementation("org.hibernate.orm:hibernate-vector")
     runtimeOnly(libs.h2)
-    runtimeOnly(libs.mysql.connector.j)
+//    runtimeOnly(libs.mysql.connector.j)
 
     implementation(libs.querydsl.core)
     implementation(libs.querydsl.jpa)
@@ -104,6 +107,11 @@ sourceSets {
 tasks.named("clean") {
     doLast {
         file(kaptGeneratedDir).deleteRecursively()
+    }
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.ai:spring-ai-bom:$springAiVersion")
     }
 }
 
